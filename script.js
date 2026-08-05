d51e7804a6137c6b18138c6462a2e41a257f457e
rhy806 <rhy806@gmail.com>
2026-08-05 16:33:00 +0300

    Initialize Supabase client in script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+  const SUPABASE_URL = 'https://ssriiiiuljdeftigdiyl.supabase.co';
+  const SUPABASE_ANON_KEY = 'sb_publishable_jWUTsulzCMEQV6O26zac-g_04tBxiy4';
+  const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
